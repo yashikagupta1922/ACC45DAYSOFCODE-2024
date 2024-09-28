79ed09f749395d4c90a61f062f60915f959e058b
yashikagupta1922 <145217691+yashikagupta1922@users.noreply.github.com>
2024-09-28 21:37:47 +0530

    Add files via upload

---
+++ Day1-CWC23QUALIF.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	if(x>=12){
+	    cout<<"yes"<<endl;
+	}
+	else
+	cout<<"no"<<endl;
+	
+}
