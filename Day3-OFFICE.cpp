5d9534caac8410af3f667d98075b8f459ea7e235
yashikagupta1922 <145217691+yashikagupta1922@users.noreply.github.com>
2024-09-30 19:45:53 +0530

    Add files via upload

---
+++ Day3-OFFICE.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t>0)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    cout<<4*x+y<<endl;//4 full day and 1 half day
+	    t--;
+	}
+	return 0;
+}
